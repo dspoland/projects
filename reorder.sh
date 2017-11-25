16e52a44fd3f1d0288c7dbe03701006cf4028a97
Danielle Poland <daniellespoland@gmail.com>
2017-11-25 16:02:27 -0500

    stuffs

---
+++ reorder.sh
@@ -0,0 +1,21 @@
+## end game:
+
+## download spreadsheet/file from drive
+## find items in low category
+## compare diff against previous version
+## slackbot notification of things in low that hasn't already been notified
+
+#rename previous file to old file
+
+## use python:
+>>>drive_list.py
+
+#download file as new
+
+#compare to old file
+
+#diff in toreorder.txt list
+
+# slack alert for changes
+
+#repeat on *d/*h/17m
